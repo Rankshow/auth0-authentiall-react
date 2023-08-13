fb9ff622e14c2de411bb8957a781cf9827c3b4d0
rankshow <uwaomaobinna20@gmail.com>
2023-08-13 11:33:15 +0100

    Auth0 frontend

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,24 @@ function App() {
           logout, 
           user, 
           isAuthenticated
-        } =   useAuth0;
+        } =   useAuth0();
 
   
   return (
     <>
       <div>
-        <h1>Auth0 authentication</h1>
-        <button className="bg-green-500" onClick={loginWithPopup}>Login with Popup</button>
-        <button onClick={loginWithRedirect}>Login with redirect</button>
-        <button onClick={logout}>Logout</button>
+        <h1 className="text-2xl font-bold mb-4">Auth0 authentication</h1>
+        <button className="bg-green-500 p-2 rounded-md hover:bg-slate-500 hover:text-white m-2" onClick={loginWithPopup}>Login with Popup</button>
+        <button className="bg-yellow-500 p-2 rounded-md hover:bg-slate-500 hover:text-white m-2"  onClick={loginWithRedirect}>Login with redirect</button>
+        <button className="bg-red-500 p-2 rounded-md hover:bg-slate-500 hover:text-white m-2"  onClick={logout}>Logout</button>
       </div>
-      <div>
-        <h1>User is { isAuthenticated ? "Logged in" : "Not logged in, Please register"}</h1>
-        <pre className="text-center text-teal-700 font-bold">
-          {JSON.stringify(user, null, 2)}
-        </pre>
+      <div className="mt-4">
+        <h1>User is <span className="bg-red-400 p-1 rounded-md">{ isAuthenticated ? "Logged in" : "Not logged in, Please register"}</span></h1>
+        {isAuthenticated && (
+          <pre className="text-left text-xl text-teal-700 font-light">
+            {JSON.stringify(user, null, 2)}
+          </pre>)
+        }
       </div>
     </>
   );
